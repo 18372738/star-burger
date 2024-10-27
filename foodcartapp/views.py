8387412450850f80ec12cb294d8d0363469a4457
18372738 <133884450+18372738@users.noreply.github.com>
2024-10-28 00:01:29 +0300

    Extend data validation for def register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,4 +1,5 @@
 import json
+import phonenumbers
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework import status
@@ -59,16 +60,37 @@ def product_list_api(request):
         'indent': 4,
     })
 
+
 @api_view(['POST'])
 def register_order(request):
     try:
-        if not isinstance(request.data['products'], list) or not len(request.data['products']):
+        required_fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
+
+        for field in required_fields:
+            if not request.data.get(field):
+                return Response(
+                    {
+                        'error': f'{field} - обязательное поле и не может быть пустым'
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                    )
+
+        phonenumber = request.data.get('phonenumber')
+        parsed_number = phonenumbers.parse(phonenumber, "RU")
+        if not phonenumbers.is_valid_number(parsed_number):
+            return Response(
+                {'error': 'Неверный формат телефона, должен быть для России.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not isinstance(request.data['products'], list) or not request.data['products']:
             return Response(
                 {
                     'error':  'products: Пустое значение или не список',
                 },
-                status=status.HTTP_404_NOT_FOUND
+                status=status.HTTP_400_BAD_REQUEST
             )
+
         order = Order.objects.create(
             firstname=request.data['firstname'],
             lastname=request.data['lastname'],
@@ -94,17 +116,22 @@ def register_order(request):
                     status=status.HTTP_404_NOT_FOUND
                 )
 
+    except KeyError as e:
+        return Response(
+            {'error': f'Нет данных: {str(e)}'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
     except Exception as e:
         return Response(
             {
                 'error': str(e)
             },
-            status=status.HTTP_404_NOT_FOUND
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
     return Response(
         {
             'message': 'Order created successfully',
             'order_id': order.id,
         },
-        status=status.HTTP_404_NOT_FOUND
+        status=status.HTTP_201_CREATED
     )

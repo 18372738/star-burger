7b2dfe0d4c40c24979e6a162523e7b2a4e2ba10a
18372738 <133884450+18372738@users.noreply.github.com>
2024-11-06 23:42:00 +0300

    Displayed the order cost

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from django.views import View
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.db.models import Sum, F
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
@@ -92,6 +93,8 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
+    orders = Order.objects.all()
+    order_with_total_cost = orders.annotate(total_cost=Sum(F('products__price') * F('orders__quantity')))
     return render(request, template_name='order_items.html', context={
-        'order_items': Order.objects.all()
+        'order_items': order_with_total_cost
     })
